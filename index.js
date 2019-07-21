a3aece2a786f22b852eb47256c1b9ccdd1e8f38b
Takuya KIHARA <gray@tacck.net>
2019-07-21 23:06:47 +0900

    :whale: use dokku

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({
+    headless: true,
+    args: [
+      '--no-sandbox',
+      '--disable-setuid-sandbox',
+    ]
+  });
   const page = await browser.newPage();
   await page.goto('https://osm.t88.jp/map/17/43.068/141.35');
   await page.screenshot({path: 'example.png'});
