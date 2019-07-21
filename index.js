cb63ca9e43f304ef64863aa194ae9790ff2d3933
Takuya KIHARA <gray@tacck.net>
2019-07-21 23:50:02 +0900

    :sparkles: use path

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const express = require('express')
 const app = express()
 
-app.get('/', function (req, res) {
+app.get('/:zoom/:lat/:lon', function (req, res) {
   const puppeteer = require('puppeteer');
   (async() => {
 
     const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
     const page = await browser.newPage();
-    await page.goto('https://osm.t88.jp/map/17/43.068/141.35', {waitUntil: 'networkidle2'});
+    await page.goto(`https://osm.t88.jp/map/${req.params.zoom}/${req.params.lat}/${req.params.lon}`, {waitUntil: 'networkidle2'});
     console.log(__dirname);
     await page.screenshot({path: 'ss.png'});
 
