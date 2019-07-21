2ec3fa65ca30526580e46296cd979da0812921c1
Takuya KIHARA <gray@tacck.net>
2019-07-21 23:16:27 +0900

    :whale: use dokku

--- index.js
+++ index.js
@@ -1,17 +1,33 @@
-const puppeteer = require('puppeteer');
+const express = require('express')
+const app = express()
 
-(async () => {
-  const browser = await puppeteer.launch({
-    headless: true,
-    args: [
-      '--no-sandbox',
-      '--disable-setuid-sandbox',
-    ]
-  });
-  const page = await browser.newPage();
-  await page.goto('https://osm.t88.jp/map/17/43.068/141.35');
-  await page.screenshot({path: 'example.png'});
+app.get('/', function (req, res) {
+  const puppeteer = require('puppeteer');
+  (async() => {
 
-  await browser.close();
-})();
+    const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
+    const page = await browser.newPage();
+    await page.goto('https://osm.t88.jp/map/17/43.068/141.35', {waitUntil: 'networkidle'});
+    // Type our query into the search bar
+    // await page.type('test');
+
+    // await page.click('input[type="submit"]');
+
+    // Wait for the results to show up
+    // await page.waitForSelector('h3 a');
+
+    // Extract the results from the page
+    //const links = await page.evaluate(() => {
+    //  const anchors = Array.from(document.querySelectorAll('h3 a'));
+    //  return anchors.map(anchor => anchor.textContent);
+    //});
+    await page.screenshot({path: 'example.png'});
+
+    browser.close();
+    res.send('{}');
+  })();
+})
+
+app.listen(5000, function () {
+})
 
