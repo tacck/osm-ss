fb963bf55a11bf0b34c2bb89b298af6e4d395820
Takuya KIHARA <gray@tacck.net>
2019-07-21 23:30:27 +0900

    :whale: use dokku

--- index.js
+++ index.js
@@ -8,23 +8,10 @@ app.get('/', function (req, res) {
     const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
     const page = await browser.newPage();
     await page.goto('https://osm.t88.jp/map/17/43.068/141.35', {waitUntil: 'networkidle2'});
-    // Type our query into the search bar
-    // await page.type('test');
-
-    // await page.click('input[type="submit"]');
-
-    // Wait for the results to show up
-    // await page.waitForSelector('h3 a');
-
-    // Extract the results from the page
-    //const links = await page.evaluate(() => {
-    //  const anchors = Array.from(document.querySelectorAll('h3 a'));
-    //  return anchors.map(anchor => anchor.textContent);
-    //});
-    await page.screenshot({path: 'example.png'});
+    await page.screenshot({path: 'ss.png'});
 
     browser.close();
-    res.send('{}');
+    res.sendFile('ss.png');
   })();
 })
 
